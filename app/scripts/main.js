132f516bf0720247775edf5652a55b0d28b7c428
Chase Simpson <chase@Chases-MacBook-Pro.local>
2014-10-07 17:38:56 -0400

    updates to etsy

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,13 +7,13 @@ api.results.forEach( function (whisk) {
   //Build images
   img = "<img src='" + whisk.Images[0].url_170x135 + "'/img>";
 
-  img_title = "<li>" + whisk.title + "</li>";
+  img_title = "<p class='t'>" + whisk.title + "</p>";
 
-  img_user = "<li>" + whisk.Shop.shop_name + "</li>"
+  img_user = "<p class='u'>" + whisk.Shop.shop_name + "</p>"
 
-  img_price = "<li>" + whisk.price + "</li>";
+  img_price = "<p class='p'>" + whisk.price + "</p>";
 
-  img_cc = "<li>" + whisk.currency_code + "</li>";
+  img_cc = "<p class='cc'>" + whisk.currency_code + "</p>";
 
 
   items = "<li>" + img + img_title + img_user + img_price + img_cc + "<hr /></li>";
